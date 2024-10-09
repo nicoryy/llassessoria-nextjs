2544f8cbae025a2b30d2f1136ce396fe0c51a05b
nicoryy <pedronicory@gmail.com>
2024-10-09 12:55:54 -0300

    height change

--- components/Carrousel.tsx
+++ components/Carrousel.tsx
@@ -14,9 +14,13 @@ interface CarrouselProps {
 
 const Carrousel: React.FC<CarrouselProps> = ({ items }) => {
   return (
-    <div className="carousel w-full">
+    <div className="carousel w-full h-[calc(100vh-68px)]">
       {items.map((item) => (
-        <div key={item.id} id={`slide${item.id}`} className="carousel-item relative w-full">
+        <div
+          key={item.id}
+          id={`slide${item.id}`}
+          className="carousel-item relative w-full grid place-items-center"
+        >
           <Image
             width={1920}
             height={1080}
@@ -24,10 +28,10 @@ const Carrousel: React.FC<CarrouselProps> = ({ items }) => {
             quality={80}
             src={item.img}
             alt={item.title}
-            className="w-full"
+            className="w-full absolute h-full object-cover"
           />
           <span className="bg-black/50 absolute left-0 top-0 h-full w-full" />
-          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform text-white flex-col items-center gap-5">
+          <div className="text-white flex-col items-center gap-5 justify-center transform w-96 lg:w-[70em]">
             <h1 className="text-3xl font-bold">{item.title}</h1>
             <p className="text-lg">{item.sub}</p>
           </div>
