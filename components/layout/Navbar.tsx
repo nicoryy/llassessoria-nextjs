694534ebed1bd4caead376c47f9c013447da9dc4
Nicory <pedronicory@gmail.com>
2024-10-10 12:00:23 -0300

    fix: import error

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import logo from "./../assets/logomarca.png";
+import logo from "./../../assets/logomarca.png";
 import Link from "next/link";
 
 export const Navbar = () => {
