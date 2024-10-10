2c9fbdf1a7de4f0330a3da6764a2c84c19b492cd
nicoryy <pedronicory@gmail.com>
2024-10-10 00:13:29 -0300

    title component

---
+++ components/Title.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface TitleProps {
+  title: string;
+}
+
+const Title: React.FC<TitleProps> = ({ title }) => {
+  return (
+    <div>
+      <p
+        className="text-4xl text-center font-bold font-title 
+          text-primary"
+      >
+        {title}
+      </p>
+      <hr
+        className="w-20 my-2 h-[4px] bg-accent mx-auto border-0 
+          rounded-full"
+      />
+    </div>
+  );
+};
+
+export default Title;
