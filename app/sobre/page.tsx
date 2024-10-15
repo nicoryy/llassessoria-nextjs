a9b19c8a0c409b869c5fe500753c348b0793282d
nicoryy <pedronicory@gmail.com>
2024-10-15 09:58:30 -0300

    feat: new page incoming

--- app/sobre/page.tsx
+++ app/sobre/page.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+import heroimage from "@/assets/random/low-angle-view-skyscrapers.jpg";
 
 export default function sobre() {
-  return <div>sobre</div>;
+  return (
+    <>
+      <section
+        className="h-64 font-title font-medium flex justify-start items-center pl-20 text-[#f3f3f3] bg-no-repeat bg-cover text-5xl"
+        style={{
+          backgroundColor: "rgba(0, 0, 0, 1)", // adiciona uma cor preta com opacidade 0.5
+          backgroundImage: `url(${heroimage.src})`,
+        }}
+      >
+        Sobre a L & L
+      </section>
+    </>
+  );
 }
