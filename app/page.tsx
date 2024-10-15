165fccf63dc90a0efdf4578250beb4ea31a595c0
nicoryy <pedronicory@gmail.com>
2024-10-15 02:01:26 -0300

    prettier + feat: CardBlog component

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,8 @@ import img2 from "../assets/eventos/ampliando_itarema_cascavel/img1.jpg";
 import Title from "@/components/ui/Title";
 import CardBlog from "@/components/layout/CardBlog";
 
+import { news } from "../data/news";
+
 export default function Home() {
   return (
     <>
@@ -18,15 +20,13 @@ export default function Home() {
         <Title title="Serviços" />
 
         <p className="text-normal my-8 mb-14 text-center">
-          Os serviços prestados pela L&L Assessoria é composta por profissionais
-          altamente capacitados e expertise demonstrada, atuante na área de
-          Licitações e Contratações Públicas junto a órgãos públicos
-          Prefeituras, Câmaras Municipais e Autarquias, instrui com orientações
-          voltadas para melhor aplicação junto a gestão de recursos públicos.
-          Nosso objetivo é garantir que nossos clientes estejam esclarecidos com
-          a inovação e aplicação da Nova Lei de Licitações podendo lidar com as
-          mais diversas ações nas contratações publicas de forma eficiente e
-          estratégica.
+          Os serviços prestados pela L&L Assessoria é composta por profissionais altamente
+          capacitados e expertise demonstrada, atuante na área de Licitações e Contratações Públicas
+          junto a órgãos públicos Prefeituras, Câmaras Municipais e Autarquias, instrui com
+          orientações voltadas para melhor aplicação junto a gestão de recursos públicos. Nosso
+          objetivo é garantir que nossos clientes estejam esclarecidos com a inovação e aplicação da
+          Nova Lei de Licitações podendo lidar com as mais diversas ações nas contratações publicas
+          de forma eficiente e estratégica.
         </p>
 
         <section
@@ -42,8 +42,8 @@ export default function Home() {
                 Consultoria e Assessoria Especializada
               </p>
               <p className="text-sm">
-                Esclarecemos dúvidas, fornecemos orientações e guiamos nossos
-                clientes sobre procedimentos de maneira legal e coerente
+                Esclarecemos dúvidas, fornecemos orientações e guiamos nossos clientes sobre
+                procedimentos de maneira legal e coerente
               </p>
             </div>
           </div>
@@ -56,9 +56,8 @@ export default function Home() {
                 Acompanhamento de processos
               </p>
               <p className="text-sm">
-                Utilizamos ferramentas para melhor acompanhamento de
-                procedimentos administrativos nas contratações públicos do seu
-                órgão
+                Utilizamos ferramentas para melhor acompanhamento de procedimentos administrativos
+                nas contratações públicos do seu órgão
               </p>
             </div>
           </div>
@@ -67,13 +66,10 @@ export default function Home() {
               <FaSuitcase size={25} className="text-primary" />
             </span>
             <div>
-              <p className="font-bold font-title text-xl text-primary ">
-                Rotinas Administrativas
-              </p>
+              <p className="font-bold font-title text-xl text-primary ">Rotinas Administrativas</p>
               <p className="text-sm">
-                Auxiliamos os setores envolvidos nos processos administrativos,
-                para que possam desempenhas suas ações em obediências as normas
-                e conceitos aplicados.
+                Auxiliamos os setores envolvidos nos processos administrativos, para que possam
+                desempenhas suas ações em obediências as normas e conceitos aplicados.
               </p>
             </div>
           </div>
@@ -90,9 +86,8 @@ export default function Home() {
         <Title title="Sobre a L&L" />
 
         <p className="text-normal my-5 text-center lg:mx-40">
-          Com mais de 15 anos de experiência, a L&L Assessoria se dedica a
-          fornecer serviços de consultoria personalizados que impulsionam o
-          sucesso de instituições públicas.
+          Com mais de 15 anos de experiência, a L&L Assessoria se dedica a fornecer serviços de
+          consultoria personalizados que impulsionam o sucesso de instituições públicas.
         </p>
 
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
@@ -108,23 +103,19 @@ export default function Home() {
 
           <div className="h-full flex flex-col gap-5">
             <p className="text-normal my-4 text-center">
-              Na L&L Assessoria, somos especializados em desenvolver soluções
-              sob medida para atender às necessidades exclusivas do planejamento
-              e da gestão governamental. Nossa profunda compreensão do setor
-              público, aliada ao nosso compromisso em aprimorar a eficiência
-              operacional, garante estratégias que aumentam a eficácia e os
-              resultados. Estamos comprometidos com o sucesso das instituições
-              que atendemos, oferecendo expertise que promove o crescimento e o
-              desenvolvimento no ambiente governamental.
+              Na L&L Assessoria, somos especializados em desenvolver soluções sob medida para
+              atender às necessidades exclusivas do planejamento e da gestão governamental. Nossa
+              profunda compreensão do setor público, aliada ao nosso compromisso em aprimorar a
+              eficiência operacional, garante estratégias que aumentam a eficácia e os resultados.
+              Estamos comprometidos com o sucesso das instituições que atendemos, oferecendo
+              expertise que promove o crescimento e o desenvolvimento no ambiente governamental.
             </p>
             <p className="text-normal my-4 text-center">
-              Nosso diferencial está em nossa capacidade de adaptação às
-              constantes mudanças legislativas e regulatórias. Acompanhamos de
-              perto as novas demandas do setor público, como a implementação da
-              Lei 14.133/21, garantindo que nossos clientes estejam sempre em
-              conformidade e preparados para enfrentar os desafios atuais.
-              Atuamos lado a lado com as instituições para transformar processos
-              e maximizar resultados.
+              Nosso diferencial está em nossa capacidade de adaptação às constantes mudanças
+              legislativas e regulatórias. Acompanhamos de perto as novas demandas do setor público,
+              como a implementação da Lei 14.133/21, garantindo que nossos clientes estejam sempre
+              em conformidade e preparados para enfrentar os desafios atuais. Atuamos lado a lado
+              com as instituições para transformar processos e maximizar resultados.
             </p>
           </div>
         </div>
@@ -145,10 +136,9 @@ export default function Home() {
             </h2>
 
             <p className="hidden text-gray-500 md:mt-4 md:block">
-              Está com dúvidas sobre a nova Lei de Licitações? Fale conosco
-              agora! Nossa equipe está pronta para assessorar você em cada etapa
-              dos procedimentos administrativos. Entre em contato e obtenha o
-              suporte que sua empresa precisa.
+              Está com dúvidas sobre a nova Lei de Licitações? Fale conosco agora! Nossa equipe está
+              pronta para assessorar você em cada etapa dos procedimentos administrativos. Entre em
+              contato e obtenha o suporte que sua empresa precisa.
             </p>
 
             <div className="mt-4 md:mt-8">
@@ -182,9 +172,16 @@ export default function Home() {
         <Title title="Noticias" />
 
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-10 my-8">
-          <CardBlog  />
-          <CardBlog />
-          <CardBlog />
+          {news.map((item) => (
+            <CardBlog
+              key={item.id}
+              title={item.title}
+              description={item.desc}
+              img={item.img}
+              data={item.data}
+              link={item.link}
+            />
+          ))}
         </div>
       </section>
     </>
