383b0bd4460ba0d6b136fe399ad57637c8b614fc
nicoryy <pedronicory@gmail.com>
2024-10-15 08:52:27 -0300

    chore: delete non used lines

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,17 +5,6 @@ import FloatMenu from "../components/ui/FloatMenu";
 import { ThemeProvider } from "@/components/util/context/ThemeProvider";
 import Footer from "@/components/layout/Footer";
 
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
-
 export const metadata: Metadata = {
   title: "L&L Assessoria e Serviços",
   description:
