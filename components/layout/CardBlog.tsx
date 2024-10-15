affaee90b5f0d11c55086f41c8d0bf999eda37f7
nicoryy <pedronicory@gmail.com>
2024-10-15 09:19:34 -0300

    style: fixed height

--- components/layout/CardBlog.tsx
+++ components/layout/CardBlog.tsx
@@ -11,7 +11,7 @@ const CardBlog = ({ title, description, img, data, link }: CardBlogProps) => {
     <article className="overflow-hidden rounded-lg shadow transition hover:shadow-lg hover:shadow-base-300">
       <img alt={title} src={img} className="h-56 w-full object-cover" />
 
-      <div className="bg-base-200 p-4 sm:p-6">
+      <div className="bg-base-200 p-4 sm:p-6 h-full">
         <time dateTime="2022-10-10" className="block text-xs text-base-content">
           {" "}
           {data}{" "}
