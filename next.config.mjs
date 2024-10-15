807f317d67070030906e694ca0084d56d3304247
nicoryy <pedronicory@gmail.com>
2024-10-15 10:21:01 -0300

    fix: load images from Image next

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["tonykarlos.com", "w3.cdn.cl9.cloud", "docs.uft.edu.br"],
+  },
+};
 
 export default nextConfig;
