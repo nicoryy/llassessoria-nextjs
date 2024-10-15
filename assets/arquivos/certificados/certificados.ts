04d9c618b33d32de47546588c1d20c29769a99d2
nicoryy <pedronicory@gmail.com>
2024-10-15 10:01:46 -0300

    fix error: commented error lines

--- assets/arquivos/certificados/certificados.ts
+++ assets/arquivos/certificados/certificados.ts
@@ -1,43 +1,43 @@
-import federal from './FEDERAL ATE 16-11-2024.pdf';
-import estadual from './ESTADUAL ATE 26-07-2024.pdf';
-import municipal from './MUNICIPAL ATE 18-08-2024.pdf';
-import fgts from './FGTS ATE 11-06-2024.pdf';
-import trabalhista from './TRABALHISTA ATE 23-11-2024.pdf';
+// import federal from "./FEDERAL ATE 16-11-2024.pdf";
+// import estadual from "./ESTADUAL ATE 26-07-2024.pdf";
+// import municipal from "./MUNICIPAL ATE 18-08-2024.pdf";
+// import fgts from "./FGTS ATE 11-06-2024.pdf";
+// import trabalhista from "./TRABALHISTA ATE 23-11-2024.pdf";
 
-export const certificados = [
-    {
-        id: 1,
-        nome: 'Certidão Federal',
-        exp: '27/05/2024',
-        ate: '16/11/2024',
-        file: federal
-    },
-    {
-        id: 2,
-        nome: 'Certidão Estadual',
-        exp: '27/05/2024',
-        ate: '26/07/2024',
-        file: estadual
-    },
-    {
-        id: 3,
-        nome: 'Certidão Municipal',
-        exp: '20/05/2024',
-        ate: '18/08/2024',
-        file: municipal
-    },
-    {
-        id: 4,
-        nome: 'CRF FGTS',
-        exp: '13/05/2024',
-        ate: '11/06/2024',
-        file: fgts
-    },
-    {
-        id: 5,
-        nome: 'Certidão Trabalhista CNDT',
-        exp: '27/05/2024',
-        ate: '23/11/2024',
-        file: trabalhista
-    },
-];
+// export const certificados = [
+//   {
+//     id: 1,
+//     nome: "Certidão Federal",
+//     exp: "27/05/2024",
+//     ate: "16/11/2024",
+//     file: federal,
+//   },
+//   {
+//     id: 2,
+//     nome: "Certidão Estadual",
+//     exp: "27/05/2024",
+//     ate: "26/07/2024",
+//     file: estadual,
+//   },
+//   {
+//     id: 3,
+//     nome: "Certidão Municipal",
+//     exp: "20/05/2024",
+//     ate: "18/08/2024",
+//     file: municipal,
+//   },
+//   {
+//     id: 4,
+//     nome: "CRF FGTS",
+//     exp: "13/05/2024",
+//     ate: "11/06/2024",
+//     file: fgts,
+//   },
+//   {
+//     id: 5,
+//     nome: "Certidão Trabalhista CNDT",
+//     exp: "27/05/2024",
+//     ate: "23/11/2024",
+//     file: trabalhista,
+//   },
+// ];
