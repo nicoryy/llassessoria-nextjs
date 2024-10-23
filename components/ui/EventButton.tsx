5ed683b491b1e635df27b4e80b7c747baf6d5147
ztiepdzn <ztiepdzn@gmail.com>
2024-10-23 13:19:48 +0000

    feat: upgrade EventButton modal

--- components/ui/EventButton.tsx
+++ components/ui/EventButton.tsx
@@ -1,41 +1,37 @@
 "use client";
 
-import React, { useState } from "react";
+import { useState } from "react";
 
 const EventButton = ({ title }: { title: string }) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleClick = () => {
-    setIsOpen(true);
-  };
-
-  const handleClose = () => {
-    setIsOpen(false);
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
   };
 
   return (
     <>
-      <button
-        className="flex items-center gap-2 w-24 relative border-ppwoodland text-sm"
-        onClick={handleClick}
-      >
-        <p className="text-sm text-primary font-bold ">Veja mais →</p>
+      {/* Button to open modal */}
+      <button onClick={toggleModal} className="btn btn-primary">
+        Open Modal
       </button>
-      
+
+      {/* Modal */}
       {isOpen && (
-        <div
-          className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50"
-          onClick={handleClose}
-        >
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-3xl font-bold">
-            {title}
+        <div className="modal modal-open">
+          <div className="modal-box">
+            <h3 className="font-bold text-lg">Hello, DaisyUI Modal in TSX!</h3>
+            <p className="py-4">
+              This is a simple modal example using DaisyUI and TSX.
+            </p>
+
+            <div className="modal-action">
+              {/* Button to close modal */}
+              <button onClick={toggleModal} className="btn">
+                Close
+              </button>
+            </div>
           </div>
-          <button
-            className="absolute top-5 right-5 text-white bg-primary px-5 py-2 rounded-md"
-            onClick={handleClose}
-          >
-            Fechar
-          </button>
         </div>
       )}
     </>
@@ -43,4 +39,3 @@ const EventButton = ({ title }: { title: string }) => {
 };
 
 export default EventButton;
-
