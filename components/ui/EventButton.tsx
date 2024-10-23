adbd97c469716dd95c33d2cf7f4b2a867ac9cd2e
ztiepdzn <ztiepdzn@gmail.com>
2024-10-23 21:28:59 +0000

    feat: added gallery to events details

--- components/ui/EventButton.tsx
+++ components/ui/EventButton.tsx
@@ -1,13 +1,18 @@
 "use client";
 
+import Image, { StaticImageData } from "next/image";
 import React, { useRef } from "react";
 
 interface EventButtonProps {
   title: string;
   date: string;
+  imageList: {
+    key: number;
+    ref: StaticImageData;
+  }[];
 }
 
-const EventButton = ({ title, date }: EventButtonProps) => {
+const EventButton = ({ title, date, imageList }: EventButtonProps) => {
   const modalRef = useRef<HTMLDialogElement>(null);
 
   const openModal = () => {
@@ -18,18 +23,37 @@ const EventButton = ({ title, date }: EventButtonProps) => {
 
   return (
     <>
-      <button className="btn btn-primary text-primary-content" onClick={openModal}>
+      <button
+        className="btn btn-primary text-primary-content"
+        onClick={openModal}
+      >
         Veja mais →
       </button>
       <dialog id="my_modal_2" className="modal" ref={modalRef}>
-        <div className="modal-box">
+        <div className="modal-box  max-w-[60rem]">
           <h3 className="font-bold text-lg">{title}</h3>
           <p className="py-4">{date}</p>
 
-          {/* quero aqui */}
+          <div className="grid grid-cols-1 lg:grid-cols-4 gap-5 place-items-center">
+            {imageList.map((item) => {
+              return (
+                <Image
+                  loading="lazy"
+                  className="rounded-box w-80"
+                  src={item.ref}
+                  alt={title}
+                  key={item.key}
+                  width={300}
+                  height={200}
+                />
+              );
+            })}
+          </div>
 
-          <form method="dialog" className="btn">
-            <button>Fechar</button>
+          <form method="dialog" className="modal-close">
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
           </form>
         </div>
         <form method="dialog" className="modal-backdrop">
